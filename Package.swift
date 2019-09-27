d5aba118a6934c0379080fa5740ae8ea17de7753
Wolf McNally <wolf@wolfmcnally.com>
2019-09-27 13:57:29 -0700

    Added support for Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "WolfImage",
+    platforms: [
+        .iOS(.v9), .macOS(.v10_13), .tvOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "WolfImage",
+            targets: ["WolfImage"]),
+        ],
+    dependencies: [
+        .package(url: "https://github.com/wolfmcnally/WolfCore", from: "5.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfColor", from: "4.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfGeometry", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "WolfImage",
+            dependencies: ["WolfCore", "WolfColor", "WolfGeometry"])
+        ]
+)
--- WolfImage.podspec
+++ WolfImage.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name             = 'WolfImage'
-    s.version          = '4.0.1'
+    s.version          = '4.0.2'
     s.summary          = 'Tools for working with images in iOS and macOS.'
 
     s.homepage         = 'https://github.com/wolfmcnally/WolfImage'
